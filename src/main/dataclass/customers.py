f96673c96055a87a3a3388644542035c6670831f
IRACK <81044362+IRACK000@users.noreply.github.com>
2021-12-03 07:12:28 +0900

    Update customers.py

--- src/main/dataclass/customers.py
+++ src/main/dataclass/customers.py
@@ -30,6 +30,10 @@ class Customer(object):
         self.__orderlist: OrderList = OrderList(user_id)
         self.__total_price: int = 0
 
+    def __del__(self):
+        if self.__socket is not None:
+            self.disconnect()
+
     def get_order_list(self):
         return self.__orderlist
 
