f185a4d3bc4432a189bdd13cc0efb1c77c079dc6
irack000 <IRACK000@gmail.com>
2022-03-17 09:23:29 +0900

    Update app.py

--- src/main/app.py
+++ src/main/app.py
@@ -1,13 +1,17 @@
 # -*- coding: utf-8 -*-
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-### Alias : PosServer.app & Last Modded : 2022.01.10. ###
-Coded with Python 3.10 Grammar by purplepig4657
-Description : ?
-Reference : ?
+### Alias : PosServer.app & Last Modded : 2022.03.17. ###
+Coded with Python 3.10 Grammar by IRACK000
+Description : PosServer HTTP Server
+Reference : [create_app] https://stackoverflow.com/questions/57600034/waitress-command-line-returning-malformed-application-when-deploying-flask-web
+            [Logging] https://stackoverflow.com/questions/52372187/logging-with-command-line-waitress-serve
+            [flask] https://flask.palletsprojects.com/en/2.0.x/api/
+            [route multi rules] https://stackoverflow.com/questions/17285826/flask-redirecting-multiple-routes
+                                https://hackersandslackers.com/flask-routes/
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
-import os
-import sys
-from flask import Flask, request, jsonify
+from datetime import datetime, time
+
+from flask import Flask, request, jsonify, make_response, Request, Response, abort
 from waitress import serve
 
 if '__main__' == __name__:  # IDE가 실행 단위로 판단하지 않도록 통상적 경우와 리터럴 위치를 반대로 함.
@@ -18,27 +22,99 @@ if '__main__' == __name__:  # IDE가 실행 단위로 판단하지 않도록 통
 
 DEBUG = True
 
-app = Flask(__name__)
+
+# HTTP Error Codes
+BAD_REQUEST = 400
+UNAUTHORIZED = 401
+FORBIDDEN = 403
+NOT_FOUND = 404
+INTERNAL_SERVER_ERROR = 500
+SERVICE_UNAVAILABLE = 503
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+
+# TODO: Logging
 
 
-@app.route('/hi')
-def hi():  # put application's code here
-    return 'hi!'
+INVALID_ORDER_TOKEN_ERROR = "Invalid order token."
 
 
-@app.route('/ip', methods=['GET'])
-def name():
-    return request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
-    #return jsonify({'ip': request.remote_addr}), 200
-    #return jsonify({'ip': request.environ['REMOTE_ADDR']}), 200
+class JsonParseError(Exception):
+    def __init__(self, msg):
+        super(JsonParseError, self).__init__(msg)
+
+
+class UnauthorizedClientError(Exception):
+    def __init__(self, msg):
+        super(UnauthorizedClientError, self).__init__(msg)
+
+
+class ForbiddenAccessError(Exception):
+    def __init__(self, msg):
+        super(ForbiddenAccessError, self).__init__(msg)
+
+
+
+# < Create Flask App ------------------------------------------------------------------------------------------------->
+def create_app():
+    app = Flask(__name__)
+
+    service_denial_msg = "From 3:00 to 5:00, it is server inspection time. Sorry for the inconvenience. " \
+                         "We would appreciate it if you could try again after the inspection."
+    service_denial_start = time(3, 0, 0, 0)
+    service_denial_end = time(5, 0, 0, 0)
+
+    def server_status_noticer(func):
+        def notice_service_denial(*args, **kwargs):
+            # notice server inspection time
+            if service_denial_start <= datetime.now().time() <= service_denial_end:
+                abort(SERVICE_UNAVAILABLE, description="[ServerInspectionTimeError] " + service_denial_msg)
+            # run function with error handling
+            else:
+                try:
+                    return func(*args, **kwargs)
+                except (ValueError | KeyError | TypeError | JsonParseError) as e:
+                    abort(BAD_REQUEST, description=f"[{type(e).__name__}] {str(e)}")
+                except (OSError | RuntimeError) as e:
+                    abort(INTERNAL_SERVER_ERROR, description=f"[{type(e).__name__}] {str(e)}")
+                except UnauthorizedClientError as e:
+                    abort(UNAUTHORIZED, description=f"[{type(e).__name__}] {str(e)}")
+                except ForbiddenAccessError as e:
+                    abort(FORBIDDEN, description=f"[{type(e).__name__}] {str(e)}")
+        notice_service_denial.__name__ = func.__name__  # rename function name
+        return notice_service_denial
+
+    def parse_json(req: Request, required_key: dict[str, type] = None) -> (str, dict):
+        """
+        Parse the request json
+        :param req: Request object
+        :param required_key: required key Info (json must have this keys)
+        :return: dict when the request is valid, Response object when the request is invalid
+        """
+        personal_json = req.get_json()
+        def check_keys() -> bool:  # TODO: check if get_json returns proper type of value or just returns str type
+            for key, T in (required_key if required_key is not None else {}).items():
+                if key not in personal_json or not personal_json[key] or not isinstance(personal_json[key], T):
+                    return False
+            return True
+        if not personal_json or len(personal_json) >= len(required_key)+1 or not check_keys():
+            raise JsonParseError("Json does not contain required keys.")
+        elif 'token' not in personal_json or not isinstance(personal_json['token'], str):
+            raise UnauthorizedClientError("Authorization token is not found.")
+        else:
+            return personal_json.pop('token'), personal_json
+
+    @app.route('/')
+    @server_status_noticer
+    def index():
+        """ To check if the server is running """
+        return f"Hello, {request.environ.get('HTTP_X_REAL_IP', request.remote_addr)}!"
+
+
+
+    return app
 
 
 if __name__ == '__main__':
-    #http://daplus.net/python-flask-app-run-%EC%9D%84-%EB%8F%85%EB%A6%BD%ED%98%95%EC%9C%BC%EB%A1%9C-%EC%82%AC%EC%9A%A9%ED%95%98%EC%97%AC-%EC%97%AC%EB%9F%AC-%EA%B3%A0%EA%B0%9D%EC%97%90%EA%B2%8C-%EC%84%9C%EB%B9%84/
-    #app.run(debug=DEBUG, threaded=False, processes=3)
-    serve(app, host='0.0.0.0', port=5000, url_scheme='https')
+    wsgiapp = create_app()
+    serve(wsgiapp, host='0.0.0.0', port=5000, url_scheme='https')
