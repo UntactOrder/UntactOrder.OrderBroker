7a2b0c9489c8c4bd826d0daa4a4be2070fc6939b
irack000 <IRACK000@gmail.com>
2022-06-12 23:10:48 +0900

    Finish describing api details

--- src/main/app.py
+++ src/main/app.py
@@ -10,6 +10,7 @@ Reference : [create_app] https://stackoverflow.com/questions/57600034/waitress-c
                                 https://hackersandslackers.com/flask-routes/
 """""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 from datetime import datetime, time
+from functools import lru_cache
 
 from flask import Flask, request, jsonify, make_response, Request, Response, abort
 from waitress import serve
@@ -62,16 +63,22 @@ def create_app():
     service_denial_start = time(2, 50, 0, 0)
     service_denial_end = time(5, 10, 0, 0)
 
-    break_time_msg = "From 5:10 to 5:30, it is store break time. Sorry for the inconvenience. " \
-                     "We would appreciate it if you could try again after the break."
-    break_time_start = time(15, 30, 0, 0)
-    break_time_end = time(16, 30, 0, 0)
+    break_time_start = time(15, 30, 0, 0)  # time(15, 30, 0, 0)
+    break_time_end = time(15, 30, 0, 0)  #time(16, 30, 0, 0)
+    @lru_cache(maxsize=1)
+    def break_time_msg(break_start, break_end):
+        return f"From {f'{break_start}'[:-3]} to {f'{break_end}'[:-3]}, it is store break time. " \
+               f"Sorry for the inconvenience. We would appreciate it if you could try again after the break."
 
     def server_status_noticer(func):
         def notice_service_denial(*args, **kwargs):
             # notice server inspection time
             if service_denial_start <= datetime.now().time() <= service_denial_end:
                 abort(SERVICE_UNAVAILABLE, description="[ServerInspectionTimeError] " + service_denial_msg)
+            # notice store break time
+            if not break_time_start == break_time_end and break_time_start <= datetime.now().time() <= break_time_end:
+                abort(SERVICE_UNAVAILABLE, description="[StoreBreakTimeError] "
+                                                       + break_time_msg(break_time_start, break_time_end))
             # run function with error handling
             else:
                 try:
@@ -122,6 +129,11 @@ def create_app():
         """ Process the sign in or sign up request (CUSTOMER) - POST method
             Request:
                 Body = {token: user_order_token, password: MD5(table_password)}
+            Response:
+                200 Login Success - Login Succeed
+                401 Unauthorized  - User Order Token Not Found
+                403 Forbidden     - Password Invalid
+                404 Not Found     - Table Not Exists
         """
         parsed_json = parse_json(request)
         ap.process_sign_in_or_up(parsed_json[0], **parsed_json[1])
@@ -136,13 +148,13 @@ def create_app():
     @app.patch('/sign')  # additional login process
     @server_status_noticer
     def process_admin_sign_in_or_up() -> Response:
-        """ Process the sign in or sign up request (ADMIN) - POST method
+        """ Process the sign in or sign up request (ADMIN) - Put/Patch method
             Request:
                 if method is put:
-                    Body = {token: firebase_id_token, password: MD5(table_password)}
+                    Body = {token: firebase_id_token, password: MD5(cert_password)}
                 else:  # when method is patch
                     Body = {main_token: firebase_id_token (main_token), token: firebase_id_token (additional_token),
-                            password: MD5(table_password)}
+                            password: MD5(cert_password)}
             Response:
                 200 Login Success - Login Succeed
                 403 Forbidden     - Password Invalid
@@ -165,7 +177,12 @@ def create_app():
             * Check if the token belongs to ADMIN first.
             * Customer will be able to sign out only when there's no order history in customer's table.
             Request:
-                Body = {token: firebase_id_token, password: MD5(table_password)}
+                Body = if ADMIN      - {token: firebase_id_token, password: MD5(cert_password)}
+                       elif CUSTOMER - {token: order_token, password: MD5(table_password)}       <= Delete Table
+            Response:
+                200 Login Success - Login Succeed
+                401 Unauthorized  - Token Not Matched to any User/Admin
+                403 Forbidden     - Password Invalid
         """
         parsed_json = parse_json(request)
         ap.process_sign_in_or_up(parsed_json[0], **parsed_json[1])
@@ -180,33 +197,65 @@ def create_app():
     # process table order requests
     #
     @app.post('/table/<int:table_number>/status')
-    def get_table_status(table_number: int):
+    def get_table_status(table_number: int) -> Response:
         """ Get table status - POST method
             Request:
-                Body = {token: firebase_id_token}
+                Body = if ADMIN      - {token: firebase_id_token}
+                       elif CUSTOMER - {token: order_token}
             Response:
                 200 Not Found      ['status': 0]
                 200 Not Registered ['status': 1]
                 200 Not Ordered    ['status': 2]
                 200 Registered     ['status': 3]
+                401 Unauthorized  - Token Not Matched to any User/Admin
         """
         return 0
 
-    @app.post('/table/<int:table_number>/order/')
-    @app.post('/table/<int:table_number>/order_history')
-    def get_table_order_history(table_number: int):
-        """ Get table order history - POST method
+    @app.patch('/table/<int:table_number>/password')
+    def patch_table_password(table_number: int) -> Response:
+        """ Change table password. - Patch method
             Request:
-                Body = {token: firebase_id_token}
+                Body = if ADMIN      - {token: firebase_id_token,
+                                        password: MD5(cert_password), new_password: MD5(new_table_password)}
+                       elif CUSTOMER - {token: order_token,
+                                        password: MD5(table_password), new_password: MD5(new_table_password)}
+            Response:
+                200 Patch Success
+                401 Unauthorized  - User Order Token Not Found
+                403 Forbidden     - Password Invalid
+                404 Not Found     - Table Not Exists
+        """
+
+    @app.post('/table/<int:table_number>/order/', defaults={'order_index': -1})
+    @app.post('/table/<int:table_number>/order_history', defaults={'order_index': -1})
+    @app.post('/table/<int:table_number>/order/<int:order_index>')
+    def get_table_order_history(table_number: int, order_index: int) -> Response:
+        """ Get table order history - POST method
+            If order_index is -1 then get all order history. If order_index >= 0 then get specific order history.
+            Request:
+                Body = if ADMIN      - {token: firebase_id_token}
+                       elif CUSTOMER - {token: order_token}
+            Response:
+                401 Unauthorized  - User Order Token Not Found
+                404 Not Found     - Table Not Exists
+                200 Success - [{ordered_by: user_order_token, alias: order_timestamp, status: order_status,
+                               data: [{index: item_index, quantity: item_quantity, message: order_message}, ...]}, ...]
+                * order_status: int - 0(ordered), 1(accepted), 2(proceeding), 3(served), 4(cancelled), 5(purchased)
         """
         return 0
 
-
-
-    @app.put('/order')
-    def put_order():
-        """ Put order
-        :return: order token
+    @app.put('/table/<int:table_number>/order')
+    def put_new_order(table_number: int) -> Response:
+        """ Put new order - Put method
+            Request:
+                Body = if ADMIN      - {token: firebase_id_token, password: MD5(cert_password)}
+                       elif CUSTOMER - {token: order_token, password: MD5(table_password)}
+                     += {data: [{index: item_index, quantity: item_quantity, message: order_message}, ...]}, ...]}
+            Response:
+                200 Order Success, {'order_index': ?}
+                401 Unauthorized  - User Order Token Not Found
+                403 Forbidden     - Password Invalid
+                404 Not Found     - Table Not Exists
         """
         token, personal_json = parse_json(request, {'order': str})
         if token != ORDER_TOKEN:
@@ -214,18 +263,147 @@ def create_app():
         else:
             return jsonify({"token": ORDER_TOKEN})
 
-    @app.post('/table')
-    def check_table():
-        """ Post table
-        :return: table token
+    @app.post('/table/<int:table_number>/order/<int:order_index>/status')
+    def get_order_status(table_number: int, order_index: int) -> Response:
+        """ Get Order Status - Post method
+            Request:
+                Body = if ADMIN      - {token: firebase_id_token}
+                       elif CUSTOMER - {token: order_token}
+            Response:
+                200 Ordered        ['status': 0]
+                200 Accepted       ['status': 1]
+                200 Processing     ['status': 2]
+                200 Served         ['status': 3]
+                200 Cancelled      ['status': 4]
+                200 Purchased      ['status': 5]
+                401 Unauthorized  - Token Not Matched to any TableUser/Admin
+                404 Not Found     - Table Not Exists
         """
-        token, personal_json = parse_json(request, {'table': str})
-        if token != TABLE_TOKEN:
-            raise UnauthorizedClientError(INVALID_TABLE_TOKEN_ERROR)
-        else:
-            return jsonify({"token": TABLE_TOKEN})
+        return 0
 
-    return app
+    @app.patch('/table/<int:table_number>/order/<int:order_index>/status')
+    def patch_order_status(table_number: int, order_index: int) -> Response:
+        """ Patch Order Status - Patch method
+            Request:
+                Body = if ADMIN      - {token: firebase_id_token, password: MD5(cert_password), status: order_status}
+                                        if order_status is purchased then += {payment_method: payment_method}
+                                        * payment_method: int - 0~12; etc, cash, card, kakao_pay, naver_pay, payco,
+                                                                      zero_pay, paypal, paytm, phone_pay, wechat_pay,
+                                                                      ali_pay, jtnet_pay
+                       elif CUSTOMER - {token: order_token, password: MD5(table_password), status: 4}  # cancel only
+            Response:
+                200 Patch Success
+                401 Unauthorized  - Token Not Matched to any TableUser/Admin
+                403 Forbidden     - Password Invalid
+                                  - Not Allowed to Change Order Status
+                404 Not Found     - Table Not Exists
+        """
+        return 0
+
+    #
+    # store administrative requests
+    #
+    @app.patch('/table/<int:table_number>/purchase')
+    def process_purchase_request(table_number: int) -> Response:
+        """ Process purchase request (ADMIN) - Patch method
+            Request:
+                Body = {token: firebase_id_token, password: MD5(cert_password), status: order_status}
+                        if order_status is purchased then += {payment_method: payment_method}
+                        * payment_method: int - 0~12; etc, cash, card, kakao_pay, naver_pay, payco,
+                                                      zero_pay, paypal, paytm, phone_pay, wechat_pay,
+                                                      ali_pay, jtnet_pay
+            Response:
+                200 Patch Success
+                401 Unauthorized  - Unauthorized Token
+                403 Forbidden     - Password Invalid
+                404 Not Found     - Table Not Exists
+        """
+
+    @app.post('/table/status')
+    def get_all_table_status(table_number: int, order_index: int) -> Response:
+        """ Get All Table Status (ADMIN) - Post method
+            Request:
+                Body = {token: firebase_id_token}
+            Response:
+                200 Success, {0: true(; created), 1: false(; not created), ...}
+                401 Unauthorized  - Unauthorized Token
+        """
+        return 0
+
+    @app.post('/table')
+    def get_all_table_history() -> Response:
+        """ Get All Table History (ADMIN) - Post method
+            Request:
+                Body = {token: firebase_id_token}
+            Response:
+                200 Success - {0: [{ordered_by: user_order_token, alias: order_timestamp, status: order_status,
+                                   data: [{index: item_index, quantity: item_quantity, message: order_message}, ...]},
+                                   ...],
+                               ...}  ## key is table number
+                * order_status: int - 0(ordered), 1(accepted), 2(proceeding), 3(served), 4(cancelled), 5(purchased)
+        """
+        return 0
+
+    @app.patch('/break_time')
+    def set_break_time() -> Response:
+        """ Set Break Time (ADMIN) - Patch method
+            Request:
+                Body = {token: firebase_id_token, password: MD5(cert_password),
+                        break_time_start: "15:30", break_time_end: "16:30"}
+            Response:
+                200 Patch Success
+                401 Unauthorized  - Unauthorized Token
+                403 Forbidden     - Password Invalid
+        """
+        return 0
+
+    #
+    # requests related with store's menu
+    #
+    @app.post('/item', defaults={'option': "all"})
+    @app.post('/item/version', defaults={'option': "version"})
+    @app.post('/item/list', defaults={'option': "list"})
+    @app.post('/item/sold_out', defaults={'option': "sold_out"})
+    def get_item_list(option: str) -> Response:
+        """ Get Item List (ADMIN) - Post method
+            Request:
+                Body = {token: firebase_id_token or order_token}
+            Response:
+                if option is all:
+                    200 Success, {items: [{index: item_index, name: item_name, price: item_price, type: item_type,
+                                           photo: item_photo_url, description: item_description,
+                                           ingredient: item_ingredient, hashtag: item_hashtag, pinned: item_pinned}, ...],
+                                  version: item_list_version, sold_out: [item_index, ...]}
+                if option is version:
+                    200 Success, {version: item_list_version}
+                elif option is list:
+                    200 Success, [{index: item_index, name: item_name, price: item_price, type: item_type,
+                                   photo: item_photo_url, description: item_description, ingredient: item_ingredient,
+                                   hashtag: item_hashtag, pinned: item_pinned}, ...]
+                elif option is sold_out:
+                    200 Success, {sold_out: [item_index, ...]}
+                401 Unauthorized  - Unauthorized Token
+        """
+        return 0
+
+    @app.patch('/item/<int:item_index>')
+    @app.patch('/item', defaults={'item_index': -1})
+    def patch_item(item_index: int) -> Response:
+        """ Patch Item (ADMIN) - Patch method
+            When item_index is -1, it means that it is a new item - Requires all information about the item
+            When item_index is not -1, it means that it is an existing item - Requires only the information to be changed
+            Request:
+                Body = {token: firebase_id_token, password: MD5(cert_password),
+                        item_info: {name: item_name, price: item_price, type: item_type,
+                                    photo: item_photo_url, description: item_description,
+                                    ingredient: item_ingredient, hashtag: item_hashtag, pinned: item_pinned}}
+            Response:
+                200 Patch Success
+                401 Unauthorized  - Unauthorized Token
+                403 Forbidden     - Password Invalid
+                404 Not Found     - Item Not Found
+        """
+        return 0
 
 
 if __name__ == '__main__':
