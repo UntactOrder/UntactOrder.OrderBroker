da13df782eb0124737e4db7ce47c39bdcc3720f4
irack000 <IRACK000@gmail.com>
2022-08-26 23:19:45 +0900

    Init: Revert Changes

--- shared/src/commonMain/kotlin/io/github/untactorder/common/platform.kt
+++
@@ -1,3 +0,0 @@
-package io.github.untactorder.common
-
-expect fun getPlatformName(): String
--- shared/src/desktopMain/kotlin/io/github/untactorder/common/platform.kt
+++
@@ -1,5 +0,0 @@
-package io.github.untactorder.common
-
-actual fun getPlatformName(): String {
-    return "Desktop"
-}
