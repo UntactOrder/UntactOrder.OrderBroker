e3b3f99fcd2cc01ecf4f9f9dce8c32e7e6561310
irack000 <IRACK000@gmail.com>
2022-08-28 14:32:22 +0900

    Fix: Fix Dependencies

--- common/build.gradle.kts
+++ common/build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     kotlin("multiplatform")
     id("org.jetbrains.compose")
     id("com.android.library")
+    id("dev.icerock.mobile.multiplatform-resources")
 }
 
 kotlin {
@@ -16,6 +17,8 @@ kotlin {
     ).forEach {
         it.binaries.framework {
             baseName = "common"
+            export("dev.icerock.moko:resources:${rootProject.extra["moko_res_version"]}")
+            export("dev.icerock.moko:graphics:0.9.0")  // toUIColor here
         }
     }
 
@@ -28,22 +31,28 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
+                implementation(project(":shared"))
+                api(compose.ui)
+                api(compose.runtime)
                 api(compose.foundation)
                 @OptIn(org.jetbrains.compose.ExperimentalComposeLibrary::class) api(compose.material3)
+                api("dev.icerock.moko:resources:${rootProject.extra["moko_res_version"]}")
             }
         }
         val commonTest by getting {
             dependencies {
                 implementation(kotlin("test"))
+                api("dev.icerock.moko:resources-test:${rootProject.extra["moko_res_version"]}")
             }
         }
         val androidMain by getting {
             dependencies {
-                api(compose.ui)
-                api(compose.runtime)
-                implementation("androidx.compose.ui:ui-tooling-preview:${rootProject.extra["compose_ext_version"]}")
+                api("androidx.compose.ui:ui-tooling-preview:${rootProject.extra["compose_ext_version"]}")
+                api("androidx.compose.material3:material3:1.0.0-alpha16")
+                api("androidx.activity:activity-compose:1.5.1")
                 api("androidx.appcompat:appcompat:1.5.0")
                 api("androidx.core:core-ktx:1.8.0")
+                api("dev.icerock.moko:resources-compose:${rootProject.extra["moko_res_version"]}")
             }
         }
         val androidTest by getting {
@@ -72,9 +81,8 @@ kotlin {
         }
         val desktopMain by getting {
             dependencies {
-                api(compose.ui)
-                api(compose.runtime)
                 api(compose.preview)
+                api("dev.icerock.moko:resources-compose:${rootProject.extra["moko_res_version"]}")
             }
         }
         val desktopTest by getting
@@ -93,4 +101,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+}
+
+multiplatformResources {
+    multiplatformResourcesPackage = rootProject.extra["organization"] as String? + ".orderbroker.common"
 }
